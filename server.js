e39ece5cce174def8fad7f83e02a00c0107e7ce7
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-18 02:33:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content{
+    title='article one';
+    heading='This is The fist Article';
+    date='Feb 18 2018';
+    body=`            <p>
+            I am Creating mt first web app this is the first web app created by me.
+            I am Creating mt first web app this is the first web app created by me.I am Creating mt first web app this is the first web app created by me.
+            </p>`
+}
+function createTemplate(data){
+var title= data.title;
+var heading= data.heading;
+var date= data.date;
+var body=data.body;
+var htmlTemplate=`<html>
+    <head>
+         <title>
+         ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <div class= "content">
+        <div>
+            <a href="/"> Home</a>
+        
+        </div>
+       
+        <h3>
+            ${heading}
+        </h3>   
+        <div>
+          ${date}
+        </div>
+         <div>
+         ${body}
+        </div>
+    </div>
+        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article_one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'article_one.html'));
+  res.send(createTemplate(content)));
 });
 
 app.get('/ui/style.css', function (req, res) {
