4613576d70b284c5ca56637e499a7c0f047c29de
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-18 01:22:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
+  res.send( 'article_one is being displayed');
 });
 
 app.get('/ui/style.css', function (req, res) {
