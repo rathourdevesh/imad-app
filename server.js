26bb1b416288f49ab2887fe1800c962a92e1a45b
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-18 02:56:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,53 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne{
+    title='article one';
+    heading='This is The fist Article';
+    date='Feb 18 2018';
+    body=`  <p>
+            I am Creating mt first web app this is the first web app created by me.
+            I am Creating mt first web app this is the first web app created by me.I am Creating mt first web app this is the first web app created by me.
+            </p>`;
+}
+function createTemplate(data){
+var title= data.title;
+var heading= data.heading;
+var date= data.date;
+var body=data.body;
+var htmlTemplate=`<html>
+    <head>
+         <title>
+         ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <div class= "content">
+        <div>
+            <a href="/"> Home</a>
+        
+        </div>
+       
+        <h3>
+            ${heading}
+        </h3>   
+        <div>
+          ${date}
+        </div>
+         <div>
+         ${body}
+        </div>
+    </div>
+        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
 
 
 app.get('/', function (req, res) {
