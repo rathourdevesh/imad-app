33b8acf95a15a038984c89d35744f82eaf66f889
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-18 02:34:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req, res) {
-  res.send(createTemplate(content)));
+  res.send(createTemplate(content));
 });
 
 app.get('/ui/style.css', function (req, res) {
