0c72c1d12f446225eb759e5a9a4d02f92fd8d945
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-22 01:39:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,5 @@
 var button = document.getElementById('button');
 
-button.onclick = function() {
-    
-    //create a request
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var counter= request.responseText;
-                var span  = document.getElementById('count');
-                span.innerHTML = counter.toString();   
-            }
-        }
-        
-    };
-    
-    //render the variable in correct span
-  request.open('GET','http://rathourdevesh97.imad.hasura-app.io/counter',true);
-  request.send(null);
-};
-
 
 var submit = document.getElementById('submit_btn');
 submit.onclick =function() {
@@ -49,3 +28,25 @@ var name = nameInput.value;
   request.open('GET','http://rathourdevesh97.imad.hasura-app.io/submit-name?=name'+name,true);
   request.send(null);
 };
+
+button.onclick = function() {
+    
+    //create a request
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter= request.responseText;
+                var span  = document.getElementById('count');
+                span.innerHTML = counter.toString();   
+            }
+        }
+        
+    };
+    
+    //render the variable in correct span
+  request.open('GET','http://rathourdevesh97.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
+
