2672d5a485e70d29d26583b522c1c2ff58e049e3
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-21 23:51:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/article_one', function (req, res) {
 var counter=0;
 app.get('/counter',function(req,res){
     counter= counter+1;
-    res.send(counter.toString())
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
