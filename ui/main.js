895df07d8813d01fe5ba9a4d1fd39cca746432ab
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-21 23:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,21 @@ var button = document.getElementById('button');
 var counter=0;
 button.onclick =function() {
     
+    //create a request
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status=== 200){
+                var counter= request.responseText();
+             var span  = document.getElementById('count');
+             span.innerHTML = counter.toString();   
+            }
+        }
+        
+    };
     
     //render the variable in correct span
-    counter=counter+1;
-    var span  = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+  request.open('GET','http://rathourdevesh97.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
