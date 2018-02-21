0dd7f96a68ab04686eee7f17d6a4cb740ddbac8b
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-22 01:09:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,6 +58,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names=[];
+app.get('/submit-name', function(req,res){
+   var name=req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
+
+
 app.get('/article_one', function (req, res) {
   res.send(createTemplate(articleOne));
 });
@@ -72,12 +80,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var names=[];
-app.get('/submit-name', function(req,res){
-   var name=req.query.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
