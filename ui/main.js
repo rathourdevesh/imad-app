93bb2cbbc71499b1abe58926e2f6a94e0b95fb11
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2018-02-22 01:38:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,5 +47,5 @@ submit.onclick =function() {
 var name = nameInput.value;
     //render the variable in correct span
   request.open('GET','http://rathourdevesh97.imad.hasura-app.io/submit-name?=name'+name,true);
-  request.send(name);
+  request.send(null);
 };
